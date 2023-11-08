ba9c96c47005d94c7fcb5235e8126cdbecb1acb6
Cody Simms <cody.simms@sonarsource.com>
2023-11-08 13:34:41 -0600

    fix build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 sonar {
     properties {
-        property("sonar.organization", "cody-simms-sonarsource"
+        property("sonar.organization", "cody-simms-sonarsource")
         property("sonar.projectKey", "cody-simms-sonarsource_java-issues")
         property("sonar.token", "86bb61c848e53dda08c619a0f6442f6d4b5ee7a8")
         property("sonar.sources", "./src")
